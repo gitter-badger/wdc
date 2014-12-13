4b8c36bc31c6f2a72cd9b89f86a2d81f13647554
illiaKalu <illia.kalu@gmail.com>
2014-12-13 17:35:43 +0200

    make_simple_controller
    simple controller that must draw serial AM chart.

--- src/main/webapp/resources/widgets/am-bar/widget.js
+++ src/main/webapp/resources/widgets/am-bar/widget.js
@@ -3,8 +3,43 @@ define([
         '/widgets/angular-amchart/angular-amchart-directive.js'
     ],
     function (angular) {
-    var m = angular.module('app.widgets.am-bar', []);
-    m.controller('AmBarChartCtrl',
+    var app = angular.module('app.widgets.am-bar', []);
+    app.controller('AmBarChartCtrl',
            function ($scope, EventEmitter, APIProvider, APIUser) {
+
+           this.products = gems;
+           console.log('I am out of AM chart =(');
+
+           console.log(AmCharts);
+
+
+         var chart = AmCharts.makeChart( "chardiv" ,{
+         	"type": "serial",
+         	"categoryField": "category",
+         	"graphs": [
+         		{
+         			"valueField": "value"
+         		}
+         	],
+         	"dataProvider": [
+         		{
+         			"category": "category 1",
+         			"value": 8,
+         		},
+         		{
+         			"category": "category 2",
+         			"value": 4,
+         		}
+         	]
+         });
     });
+
+
+
     });
+
+  var gems = [
+     { name: 'Azurite', price: 110.50 },
+     { name: 'Bloodstone', price: 22.90 },
+     { name: 'Zircon', price: 1100 },
+   ];
